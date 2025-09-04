87d63c9df98d7f9553e0cb9c538db56533d9182c
srabonidinawal-wq <sraboni.din.awal@g.bracu.ac.bd>
2025-09-04 20:24:25 +0600

    Delete backend directory

--- backend/routes/redZoneRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRedZones } = require('../controllers/manageRedZoneController');
-
-router.get('/zones', getRedZones);
-
-module.exports = router;
--- backend/utils/sendAlert.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function sendAlertToContacts(contacts, message) {
-    // Simulating alert sending
-    contacts.forEach(contact => {
-        console.log(`ALERT SENT TO ${contact}: ${message}`);
-    });
-};
